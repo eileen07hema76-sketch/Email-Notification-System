26b990da4dfce5daa5eb5e3353e207a64b7e8723
Eileen jovita <eileen07hema76@gmail.com>
2026-07-14 19:10:16 +0530

    Implement email notification for customer registration
    Read customer names and emails from a file and send confirmation messages.

--- Email _notification.py
+++ Email _notification.py
@@ -1,1 +1,19 @@
 
+with open("Customers.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+
+        if not line or line.lower() == "customers":
+            continue
+
+        name, email = line.split(",")
+
+        print("--------------------------------")
+        print("To      :", email)
+        print("Subject :", "Confirmation Email")
+        print()
+        print(f"Hello {name},")
+        print("Your registration has been confirmed.")
+        print("Thank you!")
+        print("✅ Email sent successfully!")
+        print("--------------------------------\n")
